9e331d7ba489600d1357d22d91a138e0d50130c6
ms <200866850@qqcom>
2017-06-25 19:23:55 +0800

    测试 线程委托任务

--- redis_test.cpp
+++ redis_test.cpp
@@ -5,6 +5,15 @@
 int main(int argc, char **argv)
 {
     say_hello();
+
+    auto f1 = std::async(std::launch::async, []() {
+        say_hello();
+        return pthread_self();
+    });
+
+    printf("%lu\n", f1.get());
+
+    char x;
+    int xRet = scanf("%c", &x);
     return 1;
 }
-
