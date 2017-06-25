3a32df6a7d8d3e32a66823bcdec94fd32321cb31
ms <200866850@qqcom>
2017-06-25 18:36:44 +0800

    首次递交

---
+++ redis_test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <future>
+#include "redis_test_cmake.h"
+
+int main(int argc, char **argv)
+{
+    say_hello();
+    return 1;
+}
+
---
+++ redis_test_cmake.cpp
@@ -0,0 +1,6 @@
+#include "redis_test_cmake.h"
+
+void say_hello()
+{
+    std::cout << "Hello, from redis_test_cmake!\n";
+}
