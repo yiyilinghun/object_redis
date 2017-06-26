3cc5a9a80e0287b7d0101117aabcc98c19acc176
ms <200866850@qq.com>
2017-06-26 11:09:58 +0800

    添加redis-client c库

--- redis_test.cpp
+++ redis_test.cpp
@@ -1,20 +1,20 @@
-#include <iostream>
-#include <future>
-#include "redis_test_cmake.h"
-
-int main(int argc, char **argv)
-{
-    say_hello();
-
-    auto f1 = std::async(std::launch::async, []() {
-        say_hello();
-        //return pthread_self();
-        return 1;
-    });
-
-    printf("%d\n", f1.get());
-
-    char x;
-    int xRet = scanf("%c", &x);
-    return 1;
-}
+//#include <iostream>
+//#include <future>
+//#include "redis_test_cmake.h"
+//
+//int main(int argc, char **argv)
+//{
+//    say_hello();
+//
+//    auto f1 = std::async(std::launch::async, []() {
+//        say_hello();
+//        //return pthread_self();
+//        return 1;
+//    });
+//
+//    printf("%d\n", f1.get());
+//
+//    char x;
+//    int xRet = scanf("%c", &x);
+//    return 1;
+//}
